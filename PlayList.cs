351904f60a24851beb8648a31253428e0f323966
CattenLinger <lingerkong03@gmail.com>
2014-10-02 01:35:12 +0800

    代码更新

---
+++ PlayList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniPlayerClassic
+{
+    struct PlayListItem
+    {
+        public string FileAddress = "";
+        public string Infomations = "";
+
+        public string Singer = "";
+        public string Title = "";
+        public string Album = "";
+
+        public void refresh_info()
+        {
+            string temp = Infomations;
+
+        }
+
+        public PlayListItem()
+        {
+            if (FileAddress == "" || Infomations == "") { return; }
+            refresh_info();
+        }
+    }
+
+    public class PlayList
+    {
+
+    }
+}
--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-MiniPlayerClassic
-=================
+#MiniPlayerClassic
+---
 MiniPlayer现在正在用C#重制中。感谢这个播放器给我一段美好的回忆。
 
 本程序解码器使用Bass24 Bass.NET，请移步Un4seen官网下载相关文件。
-
-http://www.un4seen.com/
+>[Un4seen.com](http://www.un4seen.com/)
 
 解码器需要输入授权码才能关闭欢迎屏，所以需要关闭欢迎屏的话请在Un4seen官网注册自己的授权
-（请注意信息安全不要泄露自己的授权码）
+**（请注意信息安全不要泄露自己的授权码）**
 
 MiniPlayer 就像它的名字那样，提供一个简单的音频文件播放方案。
 
 
-主要结构：
+##主要结构：
 
 - 界面层(Normal MS-Style UI)
-- 中间层(Player)
+    - 自绘控件
+- 中间层
+    - 播放器(Player)
+    - 播放列表(PlayList)
 - 解码器(Bass24)
-
-
