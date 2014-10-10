92c669abd55315c2184525ffc6fd62640842078d
CattenLinger <lingerkong03@gmail.com>
2014-10-10 19:38:11 +0800

    代码更新
    因为已经忘记了链表这玩意了所以先去学习一下再回来搞定这个问题。

--- PlayList.cs
+++ PlayList.cs
@@ -5,30 +5,58 @@ using System.Text;
 
 namespace MiniPlayerClassic
 {
-    struct PlayListItem
+    public class PlayListItem
     {
-        public string FileAddress = "";
-        public string Infomations = "";
+        public string FileAddress;
+        public string Infomations;
 
-        public string Singer = "";
-        public string Title = "";
-        public string Album = "";
+        private string singer;
+        private string title;
+        private string album;
+        private string others;
+
+        public string Singer { get { return singer; } }
+        public string Title { get { return title; } }
+        public string Album { get { return album; } }
+        public string Other { get { return others; } }
+
+        public PlayListItem(string file, string info)
+        {
+            FileAddress = file;
+            Infomations = info;
+        }
 
         public void refresh_info()
         {
-            string temp = Infomations;
-
-        }
-
-        public PlayListItem()
-        {
-            if (FileAddress == "" || Infomations == "") { return; }
-            refresh_info();
+            if (Infomations == "") { return; }
+            string[] info_s = Infomations.Split(new char[] {';'});
+            singer = info_s[0];
+            title = info_s[1];
+            album = info_s[2];
+            if (info_s.Length > 3) 
+            {
+                for (byte i = 3; i < info_s.Length; i++ ) { others += info_s[i]; }
+            }
         }
     }
 
     public class PlayList
     {
+        private LinkedList<PlayListItem> list;
 
+        public PlayList()
+        { 
+            list = new LinkedList<PlayListItem>();
+        }
+
+        public void Add(string FileName, string Infomations)
+        {
+            
+        }
+
+        public void Remove(int index)
+        {
+            
+        }
     }
 }
